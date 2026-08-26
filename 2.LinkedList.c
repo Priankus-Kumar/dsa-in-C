bf68d60fdcbf27ca1efddbe43970ab30176ca6df
Priankus-Kumar <killerbutcher52@gmail.com>
2026-08-26 18:21:28 +0530

    user and manual LinkedList

--- 2.LinkedList.c
+++ 2.LinkedList.c
@@ -1,40 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
 
 struct node {
     int data;
     struct node *next;
 };
 
- void linkedListTraversal(struct node *ptr){
-    while (ptr != NULL){
-        printf("Element: %d\n", ptr->data);
-        ptr = ptr->next;
+void traverse(struct node *ptr ){
+    while(ptr!=NULL){
+        printf("element:%d\n",ptr->data);
+        ptr=ptr->next;
     }
 }
 
-int main() {
-   
 
+void manual_linkedlist(){
     struct node *head;
     struct node *second;
     struct node *third;
-
-    head = (struct node *)malloc(sizeof(struct node));
-    second = (struct node *)malloc(sizeof(struct node));
-    third = (struct node *)malloc(sizeof(struct node));
-
-    head->data = 1;
-    head->next = second;
-
-    second->data = 2;
-    second->next = third;
-
-    third->data = 3;
-    third->next = NULL;
     
-     printf("Hello World\n");
-     linkedListTraversal(head);
-
-    return 0;
+    head=(struct node *)malloc(sizeof(struct node));
+    second=(struct node *)malloc(sizeof(struct node));
+    third=(struct node *)malloc(sizeof(struct node));
+    
+    head->data=1;
+    head->next=second;
+    second->data=2;
+    second->next=third;
+    third->data=3;
+    third->next=NULL;
+    
+    traverse(head);
 }
+
+void user_linkedList(){
+    int n;
+    printf("Enter the number node you want:");
+    scanf("%d",&n);
+    struct node *head=NULL;
+    struct node *current;
+    struct node *ptr=NULL;
+    for(int a=0;a<n;a++){
+     current=(struct node *)malloc(sizeof(struct node));
+     printf("Enter the number for linked list:");
+     scanf("%d",&current->data);
+     if(head==NULL){
+         head=current;
+         ptr=current;
+     }else{
+         ptr->next=current;
+         ptr=current;
+     }
+    }
+    traverse(head);
+}
+
+ int main(){
+     
+     manual_linkedlist();
+     user_linkedList();
+     return 0;
+ }
