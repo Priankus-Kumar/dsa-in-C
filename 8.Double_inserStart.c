283ffef275c5536122992df51ec0942dc613f493
Priankus-Kumar <killerbutcher52@gmail.com>
2026-08-28 18:42:42 +0530

    third day

--- 8.Double_inserStart.c
+++ 8.Double_inserStart.c
@@ -14,12 +14,15 @@ void traverse(struct node *ptr) {
 	}
 }
 
-void backwardtraverse(struct node *ptr){
-    printf("BACKWARd");
-    while(ptr!=NULL){
-        printf("\nElement:%d\n",ptr->data);
-        ptr=ptr->prev;
-    }
+void startBegin(struct node **head,int data){
+    struct node *ptr;
+    ptr=(struct node *)malloc(sizeof(struct node));
+    
+    ptr->data=data;
+    ptr->next=*head;
+    ptr->prev=NULL;
+    
+   *head=ptr;
 }
 
 void manualLinkedlist() {
@@ -36,54 +39,27 @@ void manualLinkedlist() {
 	head->data=1;
 	head->next=second;
 	head->prev=NULL;
-
 	second->data=2;
 	second->next=third;
 	second->prev=head;
-
 	third->data=3;
 	third->next=fourth;
 	third->prev=second;
-    
 	fourth->data=4;
 	fourth->next=NULL;
 	fourth->prev=third;
 
+    startBegin(&head,5);
 	traverse(head);
+	backwardtraverse();
 
 }
 
-void userLinkedList() {
-	int n;
-	printf("Enter the number of node you want:");
-	scanf("%d",&n);
-	struct node *head=NULL;
-	struct node *ptr=NULL;
-	struct node *current;
 
-	for(int a=0; a<n; a++) {
-		current=(struct node *)malloc(sizeof(struct node));
-		printf("Enter the number:");
-		scanf("%d",&current->data);
-
-		current->prev=NULL;
-		current->next=NULL;
-
-        if(head==NULL){
-            head=current;
-        }else{
-            ptr->next=current;
-            current->prev=ptr;
-        }
-      ptr=current;
-	}
-	traverse(head);
-	backwardtraverse(ptr);
-}
 
 int main() {
 	printf("double linked list\n");
 	manualLinkedlist();
-	userLinkedList();
+// 	userLinkedList();
 	return 0;
 }
